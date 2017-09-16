7c0823b610e06adab8b04a365e25e2c205a7070a
darren <764190362@qq.com>
2017-09-16 10:25:15 +0800

    first move

---
+++ MovieStore/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using MovieStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name="darren"};
+            return View(movie);
+        }
+    }
+}
