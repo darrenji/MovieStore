b3e58d19905faaced5864b8c45d97d66050b17da
darren <764190362@qq.com>
2017-09-18 08:29:24 +0800

    eager loading

--- MovieStore/Controllers/CustomersController.cs
+++ MovieStore/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MovieStore.Controllers
 {
@@ -24,7 +25,7 @@ namespace MovieStore.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             
             return View(customers);
         }
