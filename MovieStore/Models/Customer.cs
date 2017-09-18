b6341818e5e5f5b8a2bd1811734215f3e8c354c1
darren <764190362@qq.com>
2017-09-18 19:41:42 +0800

    custom validation

--- MovieStore/Models/Customer.cs
+++ MovieStore/Models/Customer.cs
@@ -10,15 +10,17 @@ namespace MovieStore.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="必填")]
         [StringLength(255)]
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
+        //隐式约定值类型字段必须
         [Display(Name="Membership Type")]
         public byte MembershipTypeId { get; set; }
 
---
+++ MovieStore/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieStore.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if(customer.MembershipTypeId == 1)
+            {
+                return ValidationResult.Success;
+            }
+
+            if(customer.BirthDate==null)
+            {
+                return new ValidationResult("Birthdate is required");
+            }
+
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+
+            return (age >= 18) ? ValidationResult.Success : new ValidationResult("Customers should be at lease 18 years old");
+        }
+    }
+}
